aeabb70f20f235c16b9a6b31741c46e47740f21a
Prateek Verma <prateekverma1681998@gmail.com>
2025-07-09 17:50:20 +0530

    01:Setup

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const Page = () => {
+  return (
+    <div className="">
+      Hello World
+      <Button variant="destructive">click here  </Button>
+    </div>
+  );
+}
+export default Page;
