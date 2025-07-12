0277b32b3b626eb5dd87e111bd5567c3af17ebf7
Prateek Verma <prateekverma1681998@gmail.com>
2025-07-12 20:53:38 +0530

    06:E2B Sandboxes

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,16 @@
 import { inngest } from "./client";
+import {Sandbox} from  "@e2b/code-interpreter"
 import { Agent, openai,gemini, createAgent } from "@inngest/agent-kit";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
+    async({event,step}) =>{
+    const sandboxId = await step.run("get-sandbox-id", async()=>{
+        const sandbox = await Sandbox.create("vibe-prateek")
+        return sandbox.sandboxId;
+    })  
 
     const codeAgent = createAgent({
       name: "code-agent",
@@ -13,6 +19,13 @@ export const helloWorld = inngest.createFunction(
     });
 
     const { output } = await codeAgent.run(`write the following snippet : ${event.data.value}`);
-    return { output };
+    
+
+    const sandboxUrl = await step.run("get-sandbox-url", async()=>{
+        const sandbox = await getSandbox(sandboxId);
+        const host = sandbox.getHost(3000);
+        return `https://${host}`;
+    })
+    return { output , sandboxUrl};
   },
 );
