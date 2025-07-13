44bb5538b2fa4a01b179e28baca9efdf9d81a263
Prateek Verma <prateekverma1681998@gmail.com>
2025-07-13 15:19:08 +0530

    08:Messages

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,20 +1,28 @@
 import { inngest } from "./client";
 import { Sandbox } from "@e2b/code-interpreter";
-import { Agent, openai, gemini, createAgent, createTool, createNetwork } from "@inngest/agent-kit";
+import { Agent, openai, gemini, createAgent, createTool, createNetwork , type Tool} from "@inngest/agent-kit";
 import { getSandbox, lastAssistantMessageContext } from "./utils";
 import z from "zod";
 import { PROMPT } from "@/prompt";
+import { prisma } from "@/lib/db";
 
-export const helloWorld = inngest.createFunction(
-  { id: "hello-world" },
-  { event: "test/hello.world" },
+
+interface AgentState{
+  summary: string;
+  files: { [path: string]: string};
+
+}
+
+export const codeAgentFunction = inngest.createFunction(
+  { id: "code-agent" },
+  { event: "code-agent/run" },
   async ({ event, step }) => {
     const sandboxId = await step.run("get-sandbox-id", async () => {
       const sandbox = await Sandbox.create("vibe-prateek");
       return sandbox.sandboxId;
     });
 
-    const codeAgent = createAgent({
+    const codeAgent = createAgent<AgentState>({
       name: "code-agent",
       description: "An expert coding agent",
       system: PROMPT,
@@ -61,7 +69,7 @@ export const helloWorld = inngest.createFunction(
               }),
             ),
           }),
-          handler: async ({ files }, { step, network }) => {
+          handler: async ({ files }, { step, network }: Tool.Options<AgentState>  ) => {
             const updatedFiles = await step?.run("createOrUpdateFiles", async () => {
               try {
                 // Initialize the files object if it doesn't exist
@@ -126,7 +134,7 @@ export const helloWorld = inngest.createFunction(
       }
     });
 
-    const network = createNetwork({
+    const network = createNetwork<AgentState>({
       name: "coding-agent-network",
       agents: [codeAgent],
       maxIter: 15,
@@ -140,13 +148,41 @@ export const helloWorld = inngest.createFunction(
     });
 
     const result = await network.run(event.data.value);
-
+    const isError = 
+    !result.state.data.summary ||
+    Object.keys(result.state.data.files || {}).length === 0;
     const sandboxUrl = await step.run("get-sandbox-url", async () => {
       const sandbox = await getSandbox(sandboxId);
       const host = sandbox.getHost(3000);
       return `https://${host}`;
     });
+    await step.run("save-result" , async ()=>{
 
+      if( isError){
+        return await prisma.message.create({
+          data:{
+            content:"Something went wrong. Please try again.",
+            role:"ASSISTANT",
+            type:"ERROR",
+          }
+        })
+      }
+
+      return await prisma.message.create({
+        data:{
+          content: result.state.data.summary,
+          role: "ASSISTANT",
+          type: "RESULT",
+          fragment:{
+            create: {
+              sandboxUrl:sandboxUrl,
+              title:"Fragment",
+              files: result.state.data.files,
+            }
+          }
+        }
+      })
+    })
     return {
       url: sandboxUrl,
       title: "Fragment",
@@ -154,4 +190,4 @@ export const helloWorld = inngest.createFunction(
       summary: result.state.data.summary
     };
   },
-);
+); 
