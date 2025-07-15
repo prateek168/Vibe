84a1055388b1fb90d6012b10f8c1ef4f5f8c8f32
Prateek Verma <prateekverma1681998@gmail.com>
2025-07-15 12:10:46 +0530

    11:Project Header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { TRPCReactProvider } from "@/trpc/client";
 import { Toaster } from "sonner";
+import { ThemeProvider } from "next-themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,12 +28,19 @@ export default function RootLayout({
   return (
   <TRPCReactProvider>
       
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            disableTransitionOnChange
+          >
+
         <Toaster/>
         {children}
+          </ThemeProvider>
       </body>
     </html>
   </TRPCReactProvider>
--- src/modules/projects/ui/views/project-view.tsx
+++ src/modules/projects/ui/views/project-view.tsx
@@ -6,12 +6,15 @@ import{
     ResizablePanelGroup
 } from "@/components/ui/resizable"
 import { MessagesContainer } from "../components/messages-container";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
+import { Fragment } from "@/generated/prisma";
+import { ProjectHeader } from "../components/project-header";
 interface Props {
     projectId: string;
 }
 
 export const ProjectView = ({projectId}: Props)=>{ 
+    const [activeFragment , setActiveFragment] = useState<Fragment | null>(null) 
     return(
         <div className="h-screen">
             <ResizablePanelGroup direction="horizontal">
@@ -20,8 +23,16 @@ export const ProjectView = ({projectId}: Props)=>{
                 minSize={20}
                 className="flex flex-col min-h-0"
             >
+            <Suspense fallback={<p>Loading project...</p>}>
+            <ProjectHeader projectId={projectId} />
+            </Suspense>
             <Suspense fallback={<p>Loading messages...</p>}> 
-            <MessagesContainer projectId={projectId}/>
+            <MessagesContainer 
+            activeFragment={activeFragment}
+            projectId={projectId}
+            setActiveFragment = {setActiveFragment}
+            />
+            
             </Suspense>
             </ResizablePanel>
             <ResizableHandle withHandle />
